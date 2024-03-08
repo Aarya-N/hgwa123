a314754e265b870379dc3a01774cd5e4acfb6c61
Aarya-N <145162169+Aarya-N@users.noreply.github.com>
2024-03-09 01:45:23 +0530

    Add files via upload

---
+++ ADV119 hand gesture/class 119/main.js
@@ -0,0 +1,93 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quaility:90
+});
+
+camera=document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<imd id="captured_image" src="'+data_uri+'"/>';
+        hand_gestures_project_model_link.txt("https://teachablemachine.withgoogle.com/models/LVVsD6zOJ/model.json");
+    
+    })
+}
+
+console.log('ml5 version',ml5.version);
+classifier = ml5.imageClassifier('');
+
+function modelLoaded(){
+    console.log('Model Loaded!');
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The frist prediction is "+ prediction_1;
+    speak_data_2 = "And the second prediction is "+ prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
+
+function check()
+{
+    img = document.getElementById('capture_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+}
+
+if(error){
+    console.error(error);
+}
+else{
+    console.log(results);
+    document.getElementById("result_hand_name").innerHTML = results[0].label;
+    document.getElementById("result_hand_name2").innerHTML = results[1].label;
+    prediction_1 = results[0].label;
+    prediction_2 = results[1].label;
+    speak();
+    if(result[0].label == 'tumbs up'){
+        document.getElementById('update_hand_emoji').innerHTML = "&#128077";
+    }
+    if(result[0].label == 'victory'){
+        document.getElementById('update_hand_emoji').innerHTML = "&#9996";
+    }
+    if(result[0].label == 'clap'){
+        document.getElementById('update_hand_emoji').innerHTML = "&#128079";
+    }
+    if(result[0].label == 'rock'){
+        document.getElementById('update_hand_emoji').innerHTML = "&#129304";
+    }
+    if(result[0].label == 'fist'){
+        document.getElementById('update_hand_emoji').innerHTML = "&#9994";
+    }
+    if(result[0].label == 'nice'){
+        document.getElementById('update_hand_emoji').innerHTML = "&#128076";
+    }
+
+    if(result[1].label == 'tumbs up'){
+        document.getElementById('update_hand_emoji').innerHTML = "&#128077";
+    }
+    if(result[1].label == 'victory'){
+        document.getElementById('update_hand_emoji').innerHTML = "&#9996";
+    }
+    if(result[1].label == 'clap'){
+        document.getElementById('update_hand_emoji').innerHTML = "&#128079";
+    }
+    if(result[1].label == 'rock'){
+        document.getElementById('update_hand_emoji').innerHTML = "&#129304";
+    }
+    if(result[1].label == 'fist'){
+        document.getElementById('update_hand_emoji').innerHTML = "&#9994";
+    }
+    if(result[1].label == 'nice'){
+        document.getElementById('update_hand_emoji').innerHTML = "&#128076";
+    }
+
+}
